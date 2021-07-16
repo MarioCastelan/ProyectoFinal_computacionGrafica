9a610c2aa1a6024ab636b68ba777924d7a7bbc27
Mario <mario36010@gmail.com>
2021-07-16 00:09:03 -0500

    Se modifican modelos para implementar animacion
    los modelos que se van a animar son posicionados manualmente
    
    Se modificaron modelos de la habitacion y modelos de la fachada
    
    Se require posicionar manualmente, ya que cuando se posicionan en el software de Maya y se exportan para usarlos, estos tienen su pivote en el origen sin importar que este haya sido modificado en el software Maya

--- proyectoFinal/proyectoFinal/main.cpp
+++ proyectoFinal/proyectoFinal/main.cpp
@@ -47,6 +47,12 @@ bool firstMouse = true;
 GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
 
+//
+float rotar1 = 0.0;
+float rotar2 = 0.0;
+
+
+//
 
 
 int main()
@@ -141,8 +147,13 @@ int main()
     Model flor2((char*)"Models/flores/flor2.obj");
     Model flor3((char*)"Models/flores/flor3.obj");
     Model flor4((char*)"Models/flores/flor4.obj");
-    //
-    Model burbujas((char*)"Models/burbujas/burbujas.obj");
+    //Burbujas
+    Model burbuja1((char*)"Models/burbujas/burbuja.obj");
+    Model burbuja2((char*)"Models/burbujas/burbuja.obj");
+    Model burbuja3((char*)"Models/burbujas/burbuja.obj");
+    Model burbuja4((char*)"Models/burbujas/burbuja.obj");
+    Model burbuja5((char*)"Models/burbujas/burbuja.obj");
+
     Model puertaCasa((char*)"Models/puertas/puertaCasa.obj"); //El marco de la puerta ya se encuentra en el modelo de la casa
     //Ventanas de la casa, Los marcos de las ventanas ya se encuentran en el modelo de la casa
     Model ventanaCasa1((char*)"Models/ventanas/ventanaCasa1.obj");
@@ -185,7 +196,7 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glm::mat4 view;
-        //SKYBOX
+        /*_______________________________SkyBox_______________________________*/
         SkyBoxshader.Use();
         view = glm::mat4(glm::mat3(camera.GetViewMatrix()));	// Remove any translation component of the view matrix
         glUniformMatrix4fv(glGetUniformLocation(SkyBoxshader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
@@ -206,60 +217,220 @@ int main()
 
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
 
+        /*_______________________________Objetos Estaticos_______________________________*/
+        // Los objetos estaticos ya se encuentran posicionados en la escena, estos fueron
+        // posicionados desde el software Maya 
+
+        /*_____________________Habitacion_____________________*/
         habitacion.Draw(shader);
         ventanaCuarto.Draw(shader);
         trampolin.Draw(shader);
         cama.Draw(shader);
-        pelota.Draw(shader);
-        reloj.Draw(shader);
         barril.Draw(shader);
         plato.Draw(shader);
-        //Modelado jerarquico
-        jugueteAnclaje.Draw(shader);
-        juguete1.Draw(shader);
-        juguete2.Draw(shader);
-
         marcoPuertaCuarto1.Draw(shader);
-        puertaCuarto1.Draw(shader);
         marcoPuertaCuarto2.Draw(shader);
+        
+        /*_____________________Fachada_____________________*/
+        casa.Draw(shader);
+
+
+        /*_______________________________Objetos animados_______________________________*/
+        // Los objetos animados se tienen que posicionar manualmente, ya que cuando se 
+        // exporta  un objeto este no guarda el pivote fuera del origen, solo la posicion 
+        // se guarda pero el pivote se mantiene en el origen, por lo tanto se posicionan 
+        // manualmente para mantener el pivote en donde se requiera. 
+        /*_____________________Habitacion_____________________*/
+        //PuertaCuarto1
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(1.7254f, 1.7624f, -6.3046));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        puertaCuarto1.Draw(shader);
+        //PuertaCuarto2
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(1.6489f, 1.7615f, 10.9612));
+        //model = glm::rotate(model, glm::radians(rotar1), glm::vec3(0.0f, 1.0f, 0.0));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         puertaCuarto2.Draw(shader);
         
-        //Fachada
-        casa.Draw(shader);
-        //Medusa 1
-        medusa1.Draw(shader);
-        pata1_1.Draw(shader);
-        pata1_2.Draw(shader);
-        pata1_3.Draw(shader);
-        pata1_4.Draw(shader);
-        //Medusa 2
-        medusa2.Draw(shader);
-        pata2_1.Draw(shader);
-        pata2_2.Draw(shader);
-        pata2_3.Draw(shader);
-        pata2_4.Draw(shader);
-        //Flores
-        flor1.Draw(shader);
-        flor2.Draw(shader);
-        flor3.Draw(shader);
-        flor4.Draw(shader);
-        //
-        burbujas.Draw(shader);
-        puertaCasa.Draw(shader);
-        //Ventanas
-        ventanaCasa1.Draw(shader);
-        ventanaCasa2.Draw(shader);
+        //Reloj
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-5.4632f, 1.6206f, -1.7841));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        reloj.Draw(shader);
+
+        //Juguete de medusas
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(1.8922f, 9.4165f, -2.3752f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        jugueteAnclaje.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(1.2576f, 6.8221f, -3.1715f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        juguete1.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(2.4182f, 6.8540f, -1.6971f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        juguete2.Draw(shader);
+
 
         //Bob Esponja
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(2.4835f, 1.1726f, -3.6138f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         cuerpoEsponja.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(3.0852f, 1.4154f, -3.6936f));
+        model = glm::rotate(model, glm::radians(rotar1), glm::vec3(1.0f, 0.0f, 0.0));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         brazoIzquierdo.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(1.8817f, 1.4106f, -3.6926f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         brazoDerecho.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(2.7592f, 0.7526f, -3.6952f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         pieIzquierdo.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(2.2086f, 0.7526f, -3.6952f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         pieDerecho.Draw(shader);
+
+        //Pelota
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(2.2543f, 0.2781f, -2.8870f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        pelota.Draw(shader);
+
         //Gary
-        cuerpoGary.Draw(shader);
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-2.245f, 0.66f, 0.921f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        cuerpoGary.Draw(shader); 
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-1.861f, 0.168f, 1.267f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         ojoIzquierdo.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-1.921f, 0.132f, 1.303f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         ojoDerecho.Draw(shader);
+
+        /*_____________________Fachada_____________________*/
+        //Medusa1
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(16.2221f, 12.5750f, 33.5387f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        medusa1.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(15.8847, 11.2060f, 33.3054f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        pata1_1.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(15.8699f, 11.1621f, 34.3139f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        pata1_2.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(16.5612f, 11.1536f, 34.1109f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        pata1_3.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(16.7413f, 11.2340f, 33.2980f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        pata1_4.Draw(shader);
+        //Medusa2
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(21.3627f, 18.4902f, 35.5109f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        medusa2.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(20.8863f, 17.1430f, 35.4862f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        pata2_1.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(21.3452f, 17.0906f, 36.3839f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        pata2_2.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(21.8585f, 17.0477f, 35.8799f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        pata2_3.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(21.6391f, 17.1261f, 35.0766f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        pata2_4.Draw(shader);
+        
+        //Bubujas
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(17.1803f, 37.9356f, 11.6796f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        burbuja1.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(23.5857f, 38.1910f, 2.1946f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        burbuja2.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(19.7220f, 42.6049f, 11.0467f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        burbuja3.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(25.1314f, 48.2882f, -0.3447f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        burbuja4.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(22.2975f, 55.0345f, 2.0671f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        burbuja5.Draw(shader);
+
+        //Puerta casa
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-4.6333f, 6.4050f, 18.0085f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        puertaCasa.Draw(shader);
+
+        //Ventanas casa
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-8.6756f, 19.9024f, 15.4704f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        ventanaCasa1.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(8.4491f, 9.6981f, 14.8688f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        ventanaCasa2.Draw(shader);
+
+        // Flores 
+        // No necesitan posicionarse ya que su pivote ya se encuentra en donde lo queremos (el origen), para
+        // que las flores se muevan alrededor de la casa cuando cambie la luz ambiental 
+
+        flor1.Draw(shader);
+
+        flor2.Draw(shader);
+
+        flor3.Draw(shader);
+
+        flor4.Draw(shader);
 
         glfwSwapBuffers(window);
     }
@@ -290,6 +461,16 @@ void DoMovement()
     if (keys[GLFW_KEY_D] || keys[GLFW_KEY_RIGHT])
     {
         camera.ProcessKeyboard(RIGHT, deltaTime);
+    }
+
+    if (keys[GLFW_KEY_U])
+    {
+        rotar1 += 0.1;
+    }
+
+    if (keys[GLFW_KEY_I])
+    {
+        rotar1 -= 0.1;
     }
 }
 
